144f292ba4c7d1db17cef229c3a3dfbae7f506a6
clementreiffers <reiffersclement@gmail.com>
2023-03-06 17:28:53 +0100

    paintcanvas sans commentaires

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,16 +7,8 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        var paintCanvas = collision.transform.GetComponent<PaintCanvas>();
-        Vector2 coord = collision.transform.position;
-
         // Debug.Log("BOOM AT :" + coord);
         // Debug.Log("canvas : " + paintCanvas);
-        // PaintOnPaintCanvasAtHit(paintCanvas, coord);
-    }
-
-    private void PaintOnPaintCanvasAtHit(PaintCanvas paintCanvas, Vector2 coord)
-    {
-        paintCanvas.Paint(coord, 10, bulletTexture, bulletColor);
+        // 
     }
 }
